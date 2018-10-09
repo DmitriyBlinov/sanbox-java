aa7f7114208b2a7aca70f974ed097a0170f2001f
DmitriyBlinov <dmitriy.ev.blinov@gmail.com>
2018-10-09 09:42:59 +0700

    Первая версия Shapes

---
+++ CSV/src/ru/academits/blinov/csv/CSV.java
@@ -0,0 +1,28 @@
+package ru.academits.blinov.csv;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class CSV {
+    /* следующая ячейка содержит перевод строки,"до перевода строки
+    после перевода строки",а это третий столбец
+    следующая ячейка содержит кавычку и запятую,"вот они: "",",
+    в этой строке вторая и третья ячейка содержат по одной кавычке,"""",""""
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        try (PrintWriter writer = new PrintWriter("csvHTML.txt");
+             Scanner scanner = new Scanner(new FileInputStream("csv.txt"))) {
+            writer.println("<table>");
+            scanner.useDelimiter(",");
+            while (scanner.hasNextLine()) {
+                writer.print("<tr>");
+                writer.print("<td>" + scanner.next() + "</td>");
+                writer.print("</tr>");
+                writer.println("<br/>");
+            }
+            writer.print("</table>");
+        }
+    }
+}
