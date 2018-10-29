4014a28d4794cf11c731b53a5b9b1610f2504af5
DmitriyBlinov <dmitriy.ev.blinov@gmail.com>
2018-10-29 08:46:56 +0700

    Исправление ArrayListHome

---
+++ ArrayListHome/src/ru/academits/blinov/arraylisthome/ArrayListHome.java
@@ -0,0 +1,33 @@
+package ru.academits.blinov.arraylisthome;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListHome {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new FileInputStream("list.txt"))) {
+            while (scanner.hasNextInt()) {
+                numbers.add(scanner.nextInt());
+            }
+        }
+        System.out.println("Изначальная коллекция: " + numbers);
+
+        ArrayList<Integer> newNumbers = new ArrayList<>();
+        for (int e : numbers) {
+            if (!newNumbers.contains(e)) {
+                newNumbers.add(e);
+            }
+        }
+        System.out.println("Без повторений: " + newNumbers);
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) % 2 == 0) {
+                numbers.remove(i);
+                i--;
+            }
+        }
+        System.out.println("Без четных: " + numbers);
+    }
+}
