cc0ba9f98994fd83885d8f168d707f462fe83f79
Acer <dmitriy.ev.blinov@gmail.com>
2018-11-14 10:46:43 +0700

    List 1.0

--- List/src/ru/academits/blinov/list/SinglyLinkedList.java
+++ List/src/ru/academits/blinov/list/SinglyLinkedList.java
@@ -1,17 +1,4 @@
 package ru.academits.blinov.list;
-/*
-1. получение размера списка +
-2. получение значение первого элемента +
-3. получение/изменение значения по указанному индексу. ++
-Изменение значения по индексу пусть выдает старое значение.
-4. удаление элемента по индексу, пусть выдает значение элемента +
-5. вставка элемента в начало +
-6. вставка элемента по индексу +
-7. удаление узла по значению, пусть выдает true, если элемент был удален +
-8. удаление первого элемента, пусть выдает значение элемента +
-9. разворот списка за линейное время +-
-10. копирование списка +
- */
 
 import java.util.NoSuchElementException;
 
@@ -22,17 +9,14 @@ public class SinglyLinkedList<T> {
     public SinglyLinkedList() {
     }
 
-    //1. Получение размера списка
     public int getSize() {
         return count;
     }
 
-    //2. Получение значение первого элемента
     public T getHead() {
         return head.getData();
     }
 
-    //3.1 Получение значения по указанному индексу
     public T getItem(int index) {
         if (index > count || index < 0) {
             throw new NoSuchElementException("Элемента с таким индексом не существует!");
@@ -44,14 +28,13 @@ public class SinglyLinkedList<T> {
         return p.getData();
     }
 
-    //3.2 Изменение значения по указанному индексу
     public T setItem(T data, int index) {
         if (index > count - 1 || index < 0) {
             throw new NoSuchElementException("Элемента с таким индексом не существует!");
         }
         T temp = head.getData();
         if (count == 1) {
-            head = new ListItem<>(data,null);
+            head = new ListItem<>(data, null);
         } else {
             ListItem<T> p = head;
             for (int i = 0; i < index - 1; i++) {
@@ -63,7 +46,6 @@ public class SinglyLinkedList<T> {
         return temp;
     }
 
-    //4. Удаление элемента по индексу, пусть выдает значение элемента
     public T removeItem(int index) {
         if (index > count || index < 0) {
             throw new NoSuchElementException("Элемента с таким индексом не существует!");
@@ -78,7 +60,6 @@ public class SinglyLinkedList<T> {
         return temp;
     }
 
-    //5. Вставка в начало
     public void add(T data) {
         if (count == 0) {
             head = new ListItem<>(data, null);
@@ -96,7 +77,6 @@ public class SinglyLinkedList<T> {
         count++;
     }
 
-    //6. Вставка по индексу
     public void addAt(T data, int index) {
         ListItem<T> q = new ListItem<>(data);
         ListItem<T> p = head;
@@ -108,7 +88,6 @@ public class SinglyLinkedList<T> {
         count++;
     }
 
-    //7. Удаление узла по значению (удаляет все по идее)
     public boolean removeByData(T data) {
         boolean count = false;
         for (ListItem<T> p = head; p.getNext() != null; p = p.getNext()) {
@@ -121,10 +100,9 @@ public class SinglyLinkedList<T> {
         return count;
     }
 
-    //8. Удаление первого элемента
     public T removeHead() {
-        if (count <= 0) {
-            throw new NoSuchElementException("Список пуст!");
+        if (count == 0) {
+            throw new NullPointerException("Список пуст!");
         }
         T temp = head.getData();
         head = head.getNext();
@@ -132,21 +110,20 @@ public class SinglyLinkedList<T> {
         return temp;
     }
 
-    //9. Разворот списка за линейное время
     public void reverseList() {
-        if (count <= 0) {
-            throw new NoSuchElementException("Список пуст!");
+        if (count == 0) {
+            throw new NullPointerException("Список пуст!");
         }
-        int i = 0;
-        for (ListItem<T> p = head, prev = null; i < count - 1; prev = p, p = p.getNext(), i++) {
-            p.getNext().setNext(prev);
+        for (ListItem<T> p = head, prev = null, temp; p != null; prev = p, p = temp) {
+            temp = p.getNext();
+            p.setNext(prev);
+            head = p;
         }
     }
 
-    //10. Копирование спика
     public void copyLinkedList(SinglyLinkedList<T> linkedList) {
         if (linkedList.count <= 0) {
-            throw new NoSuchElementException("Список для копирования пуст!");
+            throw new NullPointerException("Список для копирования пуст!");
         }
         for (ListItem<T> p = linkedList.head; p != null; p = p.getNext()) {
             add(p.getData());
@@ -155,8 +132,8 @@ public class SinglyLinkedList<T> {
 
     @Override
     public String toString() {
-        if (count <= 0) {
-            throw new NoSuchElementException("Список пуст!");
+        if (count == 0) {
+            throw new NullPointerException("Список пуст!");
         }
         StringBuilder stringBuilder = new StringBuilder();
 
