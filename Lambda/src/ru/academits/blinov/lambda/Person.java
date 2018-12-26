f6d56a6e586958ea8b518542df55405c117c91f8
DmitriyBlinov <dmitriy.ev.blinov@gmail.com>
2018-12-26 14:52:22 +0700

    Добавлены стримы для фильтра повторений и по возрасту до 18

--- Lambda/src/ru/academits/blinov/lambda/Person.java
+++ Lambda/src/ru/academits/blinov/lambda/Person.java
@@ -16,4 +16,29 @@ public class Person {
     public String getName() {
         return name;
     }
+
+    public String toString(){
+        return getName() + "," + getAge();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        int hash = 1;
+        hash = prime * hash + name.hashCode();
+        hash = prime * hash + age;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if ((obj == null) || (obj.getClass() != this.getClass())) {
+            return false;
+        }
+        Person p = (Person) obj;
+        return name.equals(p.name) && age == p.age;
+    }
 }
--- Lambda/src/ru/academits/blinov/lambda/ru/academits/blinov/main/Main.java
+++ Lambda/src/ru/academits/blinov/lambda/ru/academits/blinov/main/Main.java
@@ -4,6 +4,8 @@ import ru.academits.blinov.lambda.Person;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,8 +14,23 @@ public class Main {
         Person phil = new Person("Phil", 44);
         Person chris = new Person("Chris", 12);
         Person daisy = new Person("Daisy", 15);
-        List<Person> persons = Arrays.asList(john, jack, phil, chris, daisy);
+        Person john2 = new Person("John", 21);
+        List<Person> persons = Arrays.asList(john, jack, phil, chris, daisy, john2);
 
-        String allNamesString = persons.stream().map()
+        Stream<Person> stream = persons.stream().distinct();
+        List<Person> filteredPersons = stream.collect(Collectors.toList());
+        String allFilteredNames = filteredPersons.stream()
+                .map(Person::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println("Имена: " + allFilteredNames);
+
+        Stream<Person> stream2 = persons.stream().filter(p -> p.getAge() < 18);
+        double averageAge = stream2
+                .mapToDouble(Person::getAge).average();
+        List<Person> youngPersons = stream2.collect(Collectors.toList());
+        System.out.println(youngPersons);
+
+        //оставить объекты, которые начинаются на al
+        //stream.filter(x -> x.getName().startsWith(“al”))
     }
 }
